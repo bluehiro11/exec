1f828812120ed2c8d42c225de8935c6dfadfd0ff
bluehiro11 <bluehiro11@hotmail.com>
2016-07-10 17:59:22 +0900

    add *

---
+++ python_ruby/Logical/2.py
@@ -0,0 +1,8 @@
+input_id = input("please input id.\n")
+input_pwd = input("please input password.\n")
+real_id = "egoing"
+real_pwd = "11"
+if real_id == input_id:
+    print("Hello!")
+else:
+    print("Who are you?")
---
+++ python_ruby/Logical/2.rb
@@ -0,0 +1,11 @@
+puts("please input id ")
+input_id = gets.chomp()
+puts("please input password")
+input_pwd = gets.chomp()
+real_id = "egoing"
+real_pwd = "11"
+if real_id==input_id and real_pwd==input_pwd
+  puts("Hello!")
+else
+  puts("Who are you?")
+end
